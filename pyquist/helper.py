b93cd31d1c820922a84e89f91679eacf9f6887f4
Chris Donahue <chrisdonahue@cmu.edu>
2024-10-09 16:10:26 -0400

    Adds frequency to pitch conversion

--- pyquist/helper.py
+++ pyquist/helper.py
@@ -9,3 +9,13 @@ def dbfs_to_amplitude(dbfs: float | np.ndarray) -> float:
 def amplitude_to_dbfs(amplitude: float | np.ndarray) -> float:
     """Converts an amplitude value to a decibel value."""
     return 20.0 * np.log10(amplitude)
+
+
+def frequency_to_pitch(frequency: float | np.ndarray) -> float:
+    """Converts a frequency value to a pitch value."""
+    return 69 + 12 * np.log2(frequency / 440.0)
+
+
+def pitch_to_frequency(pitch: float | np.ndarray) -> float:
+    """Converts a pitch value to a frequency value."""
+    return 440.0 * 2 ** ((pitch - 69) / 12)
--- pyquist/helper_test.py
+++ pyquist/helper_test.py
@@ -2,7 +2,12 @@ import unittest
 
 import numpy as np
 
-from .helper import amplitude_to_dbfs, dbfs_to_amplitude
+from .helper import (
+    amplitude_to_dbfs,
+    dbfs_to_amplitude,
+    frequency_to_pitch,
+    pitch_to_frequency,
+)
 
 
 class TestHelper(unittest.TestCase):
@@ -21,6 +26,28 @@ class TestHelper(unittest.TestCase):
             np.allclose(dbfs_to_amplitude(amplitude_to_dbfs(amplitude)), amplitude)
         )
         self.assertTrue(np.allclose(amplitude_to_dbfs(dbfs_to_amplitude(dbfs)), dbfs))
+
+    def test_frequency_pitch_conversion(self):
+        frequency = np.array([440.0, 523.251, 659.255, 783.991, 987.767])
+        pitch = np.array([69.0, 72.0, 76.0, 79.0, 83.0])
+        for f, p in zip(frequency, pitch):
+            self.assertAlmostEqual(frequency_to_pitch(f), p, places=2)
+            self.assertAlmostEqual(pitch_to_frequency(p), f, places=2)
+            self.assertAlmostEqual(
+                frequency_to_pitch(pitch_to_frequency(p)), p, places=6
+            )
+            self.assertAlmostEqual(
+                pitch_to_frequency(frequency_to_pitch(f)), f, places=6
+            )
+
+        self.assertTrue(np.allclose(frequency_to_pitch(frequency), pitch))
+        self.assertTrue(np.allclose(pitch_to_frequency(pitch), frequency))
+        self.assertTrue(
+            np.allclose(frequency_to_pitch(pitch_to_frequency(pitch)), pitch)
+        )
+        self.assertTrue(
+            np.allclose(pitch_to_frequency(frequency_to_pitch(frequency)), frequency)
+        )
 
 
 if __name__ == "__main__":
