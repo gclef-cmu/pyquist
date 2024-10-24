8e9807afe4fd371d815605b53fae44d52ddd75aa
Chris Donahue <chrisdonahue@cmu.edu>
2024-10-23 21:30:02 -0400

    Adds scientific pitch notation

--- pyquist/helper_test.py
+++ pyquist/helper_test.py
@@ -2,7 +2,13 @@ import unittest
 
 import numpy as np
 
-from .helper import dbfs_to_gain, frequency_to_pitch, gain_to_dbfs, pitch_to_frequency
+from .helper import (
+    dbfs_to_gain,
+    frequency_to_pitch,
+    gain_to_dbfs,
+    pitch_name_to_pitch,
+    pitch_to_frequency,
+)
 
 
 class TestHelper(unittest.TestCase):
@@ -41,6 +47,36 @@ class TestHelper(unittest.TestCase):
         self.assertTrue(
             np.allclose(pitch_to_frequency(frequency_to_pitch(frequency)), frequency)
         )
+
+    def test_pitch_name_to_pitch(self):
+        self.assertEqual(pitch_name_to_pitch("B3"), 59)
+        self.assertEqual(pitch_name_to_pitch("C4"), 60)
+        self.assertEqual(pitch_name_to_pitch("D4"), 62)
+        self.assertEqual(pitch_name_to_pitch("E4"), 64)
+        self.assertEqual(pitch_name_to_pitch("F4"), 65)
+        self.assertEqual(pitch_name_to_pitch("G4"), 67)
+        self.assertEqual(pitch_name_to_pitch("A4"), 69)
+        self.assertEqual(pitch_name_to_pitch("B4"), 71)
+        self.assertEqual(pitch_name_to_pitch("C5"), 72)
+
+        self.assertEqual(pitch_name_to_pitch("Bb3"), 58)
+        self.assertEqual(pitch_name_to_pitch("Bbbbb3"), 55)
+        self.assertEqual(pitch_name_to_pitch("C#4"), 61)
+        self.assertEqual(pitch_name_to_pitch("C####4"), 64)
+        self.assertEqual(pitch_name_to_pitch("c4"), 60)
+
+        self.assertEqual(
+            pitch_name_to_pitch(np.array(["B3", "C4", "D4"])).tolist(), [59, 60, 62]
+        )
+
+        with self.assertRaises(ValueError):
+            pitch_name_to_pitch("A")
+        with self.assertRaises(ValueError):
+            pitch_name_to_pitch("Z3")
+        with self.assertRaises(ValueError):
+            pitch_name_to_pitch("Cd3")
+        with self.assertRaises(ValueError):
+            pitch_name_to_pitch("BK")
 
 
 if __name__ == "__main__":
