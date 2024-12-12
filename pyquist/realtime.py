ad115d8b8b9de720d677a13daae0acf28587f3f5
Chris Donahue <chrisdonahue@cmu.edu>
2024-12-11 23:15:54 -0500

    Working on realtime support

--- pyquist/realtime.py
+++ pyquist/realtime.py
@@ -1,7 +1,36 @@
 import abc
-from typing import Optional
+from dataclasses import dataclass
+from typing import Any, Iterator, List, Optional, Tuple
 
-from .audio import AudioBuffer
+from .audio import Audio, AudioBuffer
+
+
+@dataclass
+class BlockMessage:
+    """A real-time control input to an audio processor.
+
+    Can come from a variety of sources, such as MIDI, OSC, or a GUI.
+
+    Attributes:
+        offset: The offset in samples from the start of the block.
+        data: The message data.
+    """
+
+    data: Any
+    offset: int = 0
+
+
+@dataclass
+class Message:
+    """A message timestamped in seconds, mainly used for scheduling.
+
+    Attributes:
+        time: The time in seconds.
+        data: The message data.
+    """
+
+    data: Any
+    time: float
 
 
 class AudioProcessor(abc.ABC):
@@ -13,50 +42,275 @@ class AudioProcessor(abc.ABC):
     https://docs.juce.com/master/classAudioProcessor.html
     """
 
-    def __init__(self):
+    def __init__(self, num_input_channels: int = 0, num_output_channels: int = 1):
         """Initializes the audio processor."""
-        self._sample_rate = None
-        self._block_size = None
+        self._sample_rate: Optional[int] = None
+        self._block_size: Optional[int] = None
         self._prepared = False
+        self._num_input_channels = num_input_channels
+        self._num_output_channels = num_output_channels
 
     @property
-    def prepared(self):
+    def num_input_channels(self) -> int:
+        """Returns the number of input channels."""
+        return self._num_input_channels
+
+    @property
+    def num_output_channels(self) -> int:
+        """Returns the number of output channels."""
+        return self._num_output_channels
+
+    @property
+    def prepared(self) -> bool:
         """Returns True if the processor is prepared for playback."""
         return self._prepared
 
     @property
-    def sample_rate(self):
-        """Returns the sample rate of the audio processor."""
+    def sample_rate(self) -> int:
+        """Once prepared, returns the sample rate of the AudioProcessor."""
         if not self.prepared:
             raise RuntimeError("Audio processor is not prepared for playback")
+        assert self._sample_rate is not None
         return self._sample_rate
 
     @property
-    def block_size(self):
-        """Returns the block size of the audio processor."""
+    def block_size(self) -> int:
+        """Once prepared, returns the block size of the AudioProcessor."""
         if not self.prepared:
             raise RuntimeError("Audio processor is not prepared for playback")
+        assert self._block_size is not None
         return self._block_size
 
-    def prepare_to_play(self, sample_rate: float, block_size: int):
+    def prepare(self, sample_rate: int, block_size: int):
         """Prepares the processor for playback with the given sample rate and block size."""
         self._sample_rate = sample_rate
         self._block_size = block_size
         self._prepared = True
 
-    def release_resources(self):
+    def release(self):
         """Releases any resources that are no longer needed."""
         self._sample_rate = None
         self._block_size = None
         self._prepared = False
 
     @abc.abstractmethod
-    def process_block(self, buffer: Optional[AudioBuffer]):
-        """Processes the audio block in the buffer."""
-        pass
+    def process_block(self, buffer: AudioBuffer, messages: List[BlockMessage]):
+        """Override with your block processing logic.
 
-    def __call__(self, buffer: Optional[AudioBuffer]):
-        """Processes the audio block in the buffer."""
+        The audio buffer will be of shape `(num_channels, block_size)`, where
+        `num_channels = max(num_input_channels, num_output_channels)`. The first
+        `num_input_channels` channels will contain the input audio, and the first
+        `num_output_channels` channels should be written with the output audio.
+
+        Args:
+            buffer: The audio buffer to process in place.
+            messages: The block messages to process. Assume they are sorted by offset.
+        """
+
+    def __call__(self, buffer: AudioBuffer, messages: List[BlockMessage]):
+        """Processes a block of audio with the given messages."""
         if not self.prepared:
             raise RuntimeError("Audio processor is not prepared for playback")
-        self.process_block(buffer)
+        self.process_block(buffer, messages)
+
+
+def iter_process(
+    duration: float,
+    processor: AudioProcessor,
+    input_audio: Optional[Audio] = None,
+    messages: Optional[List[Message]] = None,
+    *,
+    pad_end: bool = True,
+    block_size: int = 512,
+    sample_rate: int = 44100,
+) -> Iterator[Tuple[AudioBuffer, List[BlockMessage]]]:
+    """Iteratively processes audio with the given audio processor.
+
+    Yields audio blocks as they are processed.
+
+    Args:
+        processor: The audio processor to use.
+        duration: The duration of the audio in seconds.
+        input_audio: The input audio to process.
+        messages: The scheduled messages to send to the processor.
+        pad_end: Whether to pad the end of the audio with zeros.
+        block_size: The block size to use for processing.
+        sample_rate: The sample rate to use for processing.
+
+    Yields:
+        AudioBuffer: The processed audio block.
+    """
+    if messages is None:
+        messages = []
+    if input_audio is None and processor.num_input_channels > 0:
+        raise ValueError("Input audio is required for this processor")
+
+    processor.prepare(sample_rate, block_size)
+    num_samples = int(duration * sample_rate)
+    block = AudioBuffer(
+        num_channels=max(processor.num_input_channels, processor.num_output_channels),
+        num_samples=block_size,
+    )
+    messages = sorted(messages, key=lambda m: m.time)
+
+    for i in range(0, num_samples, block_size):
+        # Handle incomplete blocks
+        block_i_size = num_samples - i
+        if block_i_size < block_size and not pad_end:
+            break
+
+        # Handle scheduled messages
+        block_messages = []
+        block_i_time = i / sample_rate
+        block_ip1_time = (i + block_size) / sample_rate
+        while messages and messages[0].time < block_ip1_time:
+            message = messages.pop(0)
+            offset = int((message.time - block_i_time) * sample_rate)
+            assert 0 <= offset < block_size
+            block_messages.append(BlockMessage(offset=offset, data=message.data))
+
+        # Buffer input audio
+        if input_audio is not None:
+            input_block = input_audio[
+                : processor.num_input_channels, i : i + block_i_size
+            ]
+            block[: input_block.shape[0], : input_block.shape[1]] = input_block
+
+        # Compute output audio
+        processor(block, block_messages)
+
+        # Pad end of block with zeros if needed
+        if block_i_size < block_size:
+            block[: processor.num_output_channels, block_i_size:] = 0.0
+
+        yield block[: processor.num_output_channels], block_messages
+
+
+def process(
+    duration: float,
+    processor: AudioProcessor,
+    input_audio: Optional[Audio] = None,
+    messages: Optional[List[Message]] = None,
+    *,
+    block_size: int = 512,
+    sample_rate: int = 44100,
+) -> Audio:
+    num_samples = int(duration * sample_rate)
+    output = Audio(
+        num_channels=processor.num_output_channels,
+        num_samples=num_samples,
+        sample_rate=sample_rate,
+    )
+    for i, (block, _) in enumerate(
+        iter_process(
+            duration=duration,
+            processor=processor,
+            input_audio=input_audio,
+            messages=messages,
+            block_size=block_size,
+            sample_rate=sample_rate,
+            pad_end=True,
+        )
+    ):
+        block_offset = i * block_size
+        output[
+            : processor.num_output_channels,
+            block_offset : block_offset + block.shape[1],
+        ] = block[:, : block.shape[1]]
+    return output
+
+
+"""
+
+
+class AudioStream(sd.OutputStream):
+    def __init__(
+        self,
+        processor: AudioProcessor,
+        *,
+        block_size: int = 512,
+        sample_rate: int = 44100,
+    ):
+        super().__init__(
+            samplerate=sample_rate,
+            blocksize=block_size,
+            channels=(processor.num_input_channels, processor.num_output_channels),
+            callback=self.callback,
+        )
+        self.processor = processor
+        self.block_size = block_size
+        self.sample_rate = sample_rate
+        self.message_queue = deque()
+
+    def callback(self, outdata, *args):
+        del args
+        messages = [
+            self.message_queue.popleft() for _ in range(len(self.message_queue))
+        ]
+        buffer = AudioBuffer(
+            num_channels=max(
+                self.processor.num_input_channels, self.processor.num_output_channels
+            ),
+            num_samples=self.block_size,
+            array=outdata.swapaxes(0, 1),
+        )
+        self.processor(buffer, messages)
+
+    def __enter__(self, *args, **kwargs):
+        self.processor.prepare_to_play(self.sample_rate, self.block_size)
+        super().__enter__(*args, **kwargs)
+        return self.message_queue
+
+    def __exit__(self, *args, **kwargs):
+        super().__exit__(*args, **kwargs)
+        self.processor.release_resources()
+
+
+def process(
+    processor: AudioProcessor,
+    *,
+    block_size: int = 512,
+    sample_rate: int = 44100,
+    keyboard_control: bool = False,
+    scheduled_messages: List[Tuple[float, Any]] = [],
+    duration: Optional[float] = None,
+    synchronous: bool = False,
+) -> None | Audio:
+    #Processes audio with the given audio processor.
+    if keyboard_control:
+        import pygame
+        from collections import deque
+
+        # Initialize Pygame
+        pygame.init()
+        screen = pygame.display.set_mode(size=(400, 300))
+        pygame.display.set_caption("Press any key")
+
+        font = pygame.font.SysFont(name=None, size=24)
+        text = font.render(
+            "Press keyboard keys to send messages", True, (255, 255, 255)
+        )
+
+        processor.prepare_to_play(sample_rate, block_size)
+    else:
+
+        def audio_callback(outdata, *args):
+            del args
+            buffer = AudioBuffer(
+                num_channels=1, num_samples=block_size, array=outdata.swapaxes(0, 1)
+            )
+            processor(buffer, [])
+            return None
+
+    if duration is None:
+        pass
+    else:
+        with sd.OutputStream(
+            samplerate=sample_rate,
+            blocksize=block_size,
+            channels=1,
+            callback=audio_callback,
+        ):
+            sd.sleep(int(duration * 1000))
+
+"""
--- pyquist/realtime_test.py
+++ pyquist/realtime_test.py
@@ -1,25 +1,136 @@
 import unittest
 
-from .realtime import AudioProcessor
+# from math import isclose
+from typing import List
+
+import numpy as np
+
+from .audio import Audio, AudioBuffer
+from .realtime import AudioProcessor, BlockMessage, iter_process, process
 
 
-class TestAudioProcessor(unittest.TestCase):
+class SineAudioProcessor(AudioProcessor):
+    def __init__(self):
+        super().__init__(num_input_channels=1, num_output_channels=1)
+
+    def process_block(self, buffer: AudioBuffer, messages: List[BlockMessage]):
+        del messages  # Unused
+        np.sin(buffer, out=buffer)
+
+
+class MessageAudioProcessor(AudioProcessor):
+    def __init__(self):
+        super().__init__(num_input_channels=0, num_output_channels=1)
+        self.current_value = 0.0
+
+    def process_block(self, buffer: AudioBuffer, messages: List[BlockMessage]):
+        for i in range(buffer.shape[1]):
+            while messages and messages[0].offset == i:
+                new_value = messages[0].data
+                if not isinstance(new_value, float):
+                    raise TypeError("Expected float message data")
+                self.current_value = new_value
+                messages.pop(0)
+            buffer[0, i] = self.current_value
+
+
+class TestAudioProcessing(unittest.TestCase):
+
     def test_audio_processor(self):
-        class TestProcessor(AudioProcessor):
-            def process_block(self, buffer):
-                pass
+        processor = SineAudioProcessor()
+        self.assertEqual(processor.num_input_channels, 1)
+        self.assertEqual(processor.num_output_channels, 1)
+        self.assertFalse(processor.prepared)
 
-        processor = TestProcessor()
-        self.assertIsNone(processor.sample_rate)
-        self.assertIsNone(processor.block_size)
-        processor.prepare_to_play(44100, 512)
+    def test_prepare_to_play(self):
+        processor = SineAudioProcessor()
+        processor.prepare(sample_rate=44100, block_size=512)
+        self.assertTrue(processor.prepared)
         self.assertEqual(processor.sample_rate, 44100)
         self.assertEqual(processor.block_size, 512)
-        processor.release_resources()
-        processor.process_block(None)
 
-        with self.assertRaises(TypeError):
-            AudioProcessor()
+    def test_release_resources(self):
+        processor = SineAudioProcessor()
+        processor.prepare(sample_rate=44100, block_size=512)
+        processor.release()
+        self.assertFalse(processor.prepared)
+        with self.assertRaises(RuntimeError):
+            processor.sample_rate
+        with self.assertRaises(RuntimeError):
+            processor.block_size
+
+    def test_iter_process_sine(self):
+        # Test parameters
+        sample_rate = 44100
+        duration = 0.01  # short duration for testing
+        frequency = 440.0
+
+        # Create input: a phasor that increases linearly in time:
+        # phase(t) = 2π * f * t
+        num_samples = int(sample_rate * duration)
+        t = np.linspace(0, duration, num_samples, endpoint=False)
+        phase = 2 * np.pi * frequency * t
+        input_audio = Audio(
+            num_channels=1, num_samples=num_samples, sample_rate=sample_rate
+        )
+        input_audio[0, :] = phase
+
+        processor = SineAudioProcessor()
+        block_size = 512
+
+        # No scheduled messages for this test
+        messages = []
+
+        # Run iter_process
+        blocks = list(
+            iter_process(
+                processor=processor,
+                duration=duration,
+                input_audio=input_audio,
+                messages=messages,
+                block_size=block_size,
+                sample_rate=sample_rate,
+            )
+        )
+
+        # Check that blocks are of expected shape and that processor outputs sine
+        output_data = np.hstack(
+            [blk[0] for blk in blocks]
+        )  # blk is (buffer, block_messages)
+        self.assertEqual(output_data.shape[0], num_samples)
+
+        # Compare output to np.sin(phase)
+        expected = np.sin(phase)
+        # Allow a small numerical tolerance
+        self.assertTrue(np.allclose(output_data, expected, atol=1e-6))
+
+    def test_process_sine(self):
+        # Test parameters
+        sample_rate = 44100
+        duration = 0.01  # short duration for testing
+        frequency = 440.0
+
+        # Create input phasor
+        num_samples = int(sample_rate * duration)
+        t = np.linspace(0, duration, num_samples, endpoint=False)
+        phase = 2 * np.pi * frequency * t
+        input_audio = Audio(
+            num_channels=1, num_samples=num_samples, sample_rate=sample_rate
+        )
+        input_audio[0, :] = phase
+
+        processor = SineAudioProcessor()
+        output = process(
+            processor=processor,
+            duration=duration,
+            input_audio=input_audio,
+            block_size=512,
+            sample_rate=sample_rate,
+        )
+
+        # Check output matches expected sine
+        expected = np.sin(phase)
+        self.assertTrue(np.allclose(output[0, :], expected, atol=1e-6))
 
 
 if __name__ == "__main__":
