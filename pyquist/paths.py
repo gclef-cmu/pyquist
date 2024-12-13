029190b836aa83dbc102d36c7f55af629f940bee
Chris Donahue <chrisdonahue@cmu.edu>
2024-12-12 22:27:22 -0500

    Adds cacheing to freesound

--- pyquist/paths.py
+++ pyquist/paths.py
@@ -6,7 +6,7 @@ LIB_DIR = pathlib.Path(__file__).resolve().parent
 if "PYQUIST_CACHE_DIR" in os.environ:
     CACHE_DIR = pathlib.Path(os.environ["PYQUIST_CACHE_DIR"])
 else:
-    CACHE_DIR = pathlib.Path(pathlib.Path.home(), ".pyquist")
+    CACHE_DIR = pathlib.Path(pathlib.Path.home(), ".cache", "pyquist")
 CACHE_DIR = CACHE_DIR.resolve()
 CACHE_DIR.mkdir(parents=True, exist_ok=True)
 
