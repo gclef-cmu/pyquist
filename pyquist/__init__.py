67fa3bc4960fd192159e49fbdb5ba2e575769208
Chris Donahue <chrisdonahue@users.noreply.github.com>
2024-12-12 20:39:57 -0500

    Adds unittest discovery (#29)

--- pyquist/__init__.py
+++ pyquist/__init__.py
@@ -1,4 +1,5 @@
 from .audio import Audio, AudioBuffer
+from .paths import LIB_DIR
 from .realtime import AudioProcessor, AudioProcessorStream
 
 __all__ = [
@@ -7,3 +8,10 @@ __all__ = [
     "AudioProcessor",
     "AudioProcessorStream",
 ]
+
+
+# NOTE: This changes the test discovery pattern from "test*.py" (default) to "*test.py".
+def load_tests(loader, standard_tests, pattern):
+    package_tests = loader.discover(start_dir=LIB_DIR, pattern="*test.py")
+    standard_tests.addTests(package_tests)
+    return standard_tests
