17d659c25f5c0ae4e0d88b6533ba59c486b391fd
Chris Donahue <chrisdonahue@users.noreply.github.com>
2024-12-12 21:36:28 -0500

    Adds tqdm (#32)

--- setup.py
+++ setup.py
@@ -3,5 +3,14 @@ from setuptools import setup
 setup(
     name="pyquist",
     packages=["pyquist", "pyquist.web"],
-    install_requires=["numpy~=2.0.2", "resampy", "soundfile", "sounddevice", "tqdm", "requests", "llvmlite>=0.43.0"],
+    install_requires=[
+        "numpy~=2.0.2",
+        "resampy",
+        "soundfile",
+        "sounddevice",
+        "tqdm",
+        "requests",
+        "llvmlite>=0.43.0",
+        "tqdm",
+    ],
 )
